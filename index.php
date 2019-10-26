8da322df242c9f38f46711b78fb4ef876dfa97f3
SandroPaulo <56982491+SandroPaulo@users.noreply.github.com>
2019-10-25 21:27:07 -0300

    Inclusão Carousel (Index.php)
    Inclusão Carousel (Index.php)

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 
+
+
     <?php include("includes/header.php"); ?>
     <style>
         .linha {
@@ -28,16 +30,43 @@
 
 <body>
 
- 
+
 
     <?php
-            include('includes/menu.php');
-            ?>
+    include('includes/menu.php');
+    ?>
 
     <div class="container text-center">
-                    
+
+    </div>
+    </div>
+
+    <div class="container">
+        <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active" data-interval="10000">
+                    <img src="assets\images\Perfumehome1.png" class="d-block" width="1100" height="520" alt="...">
+                </div>
+                <div class="carousel-item" data-interval="2000">
+                    <img src="assets\images\Perfumehome2.png" class="d-block " width="1100" height="520" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="assets\images\Perfumehome3.png" class="d-block" width="1100" height="520" alt="...">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
         </div>
     </div>
+
+
+
 
 
     <div class="container linha-palco text-center">
@@ -74,6 +103,10 @@
         </div>
     </div>
 
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 
 </html>
