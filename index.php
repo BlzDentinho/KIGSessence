2ac5a5a1f096841860ee68dcc0521324e2644be7
SandroPaulo <56982491+SandroPaulo@users.noreply.github.com>
2019-10-28 22:10:25 -0300

    Rodapé temporário Index.php

--- index.php
+++ index.php
@@ -81,6 +81,57 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
+
+
+
+    <footer id="myFooter">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3">
+                    <h2 class="logo"><a href="https://programadorviking.com.br/"> LOGO </a></h2>
+                </div>
+                <div class="col-sm-2">
+                    <h5>Inicio</h5>
+                    <ul>
+                        <li><a href="https://programadorviking.com.br/">Home</a></li>
+                        <li><a href="https://bit.ly/front-end-curso-completo-promocao-01">Cursos</a></li>
+                        <li><a href="https://programadorviking.com.br/">Downloads</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-2">
+                    <h5>Sobre-nós</h5>
+                    <ul>
+                        <li><a href="https://programadorviking.com.br/">Informações da Empresa</a></li>
+                        <li><a href="https://programadorviking.com.br/">Contato</a></li>
+                        <li><a href="https://programadorviking.com.br/">Blog</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-2">
+                    <h5>Suporte</h5>
+                    <ul>
+                        <li><a href="https://programadorviking.com.br/">FAQ</a></li>
+                        <li><a href="https://programadorviking.com.br/">Telefones</a></li>
+                        <li><a href="https://programadorviking.com.br/">Chat</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3">
+                    <div class="social-networks">
+                        <a href="https://programadorviking.com.br/" class="twitter"><i class="fa fa-twitter"></i></a>
+                        <a href="https://programadorviking.com.br/" class="facebook"><i class="fa fa-facebook"></i></a>
+                        <a href="https://programadorviking.com.br/" class="instagram"><i class="fa fa-instagram"></i></a>
+                    </div>
+                    <a href="https://programadorviking.com.br/">
+                        <button type="button" class="btn btn-default">Contato</button>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <p>© 2019 Copyright - Programador Viking</p>
+        </div>
+    </footer>
 </body>
 
 </html>
