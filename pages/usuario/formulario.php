5412bdec9f8e813a0812b327f928e9c63bc658f4
GabrielMsenac <56982472+GabrielMsenac@users.noreply.github.com>
2019-10-30 19:48:27 -0300

    Update formulario.php

--- pages/usuario/formulario.php
+++ pages/usuario/formulario.php
@@ -13,11 +13,11 @@
 
 <body>
 <form id="form-id" action="../../includes/executaform.php" method="post">
-        <div class="text-center topmenu">
-            <div class="text-center margin">Login <input type="text" name="login" class="color-text"><br></div>
-            <div class="text-center margin">Senha <input type="text" name="senha" class="color-text"><br></div>
-            <div class="text-center margin">Confirmar Senha <input type="text" name="csenha" class="color-text"><br></div>
-            <div class="text-center margin">E-mail <input type="text" name="email" class="color-text"><br></div>
+        <div class="topmenu">
+            <div class="margin">Login <input type="text" name="login" class="color-text"><br></div>
+            <div class="margin">Senha <input type="text" name="senha" class="color-text"><br></div>
+            <div class="margin">Confirmar Senha <input type="text" name="csenha" class="color-text"><br></div>
+            <div class="margin">E-mail <input type="text" name="email" class="color-text"><br></div>
             <a href="#" onclick="document.getElementById('form-id').submit();" class="btn btn-primary btn-lg text-center" role="button">Salvar</a><br>
             <img src="../../assets/images/logo.png" class="img-fundo text-center">
         </div>
