61d599e365a78bda9b09ff139cc8dcf236962890
GabrielMsenac <56982472+GabrielMsenac@users.noreply.github.com>
2019-10-31 19:24:54 -0300

    revisão do setor formulario
    formulario.php
    
    Adicionando rows

--- pages/usuario/formulario.php
+++ pages/usuario/formulario.php
@@ -13,14 +13,23 @@
 
 <body>
     <form id="form-id" action="../../includes/executaform.php" method="post">
-        <div class="topmenu text-center">
-            <div class="margin">Nome <input type="text" name="nome" class="color-text1"></div>
-            <div class="margin">Sobrenome <input type="text" name="snome" class="color-text1"></div>
-            <div class="margin">E-mail <input type="text" name="email" class="color-text1"><br></div>
-            <div class="margin">Senha <input type="text" name="senha" class="color-text1"><br></div>
-            <div class="margin">Confirmar Senha <input type="text" name="csenha" class="color-text1"><br></div>
-            <a href="#" onclick="document.getElementById('form-id').submit();" class="btn btn-primary btn-lg" role="button">Salvar</a><br>
-            <img src="../../assets/images/logo.png" class="img-fundo text-center">
+        <div class="row topmenu text-center">
+            <div class="margin col-md-6">Nome <input type="text" name="nome" class="color-text1"></div>
+            <div class="margin col-md-6">Sobrenome <input type="text" name="snome" class="color-text1"></div>
+        </div>
+        <div class="row">
+            <div class="margin col-md-12">E-mail <input type="text" name="email" class="color-text1"></div>
+        </div>
+        <div class="row">
+            <div class="margin col-md-6">Senha <input type="text" name="senha" class="color-text1"></div>
+            <div class="margin col-md-6">Confirmar Senha <input type="text" name="csenha" class="color-text1"></div>
+        </div>
+        <div class="row">
+            <div><a href="#" onclick="document.getElementById('form-id').submit();" class="col-md-12 btn btn-primary btn-lg" role="button">Salvar</a><br></div>
+        </div>
+        <div class="row">
+            <img src="../../assets/images/logo.png" class="col-md-12 img-fundo text-center">
+        </div>
         </div>
     </form>
 
