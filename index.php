1bbb7225f6cfde39904db19783c73bfa7eb34b8f
SandroPaulo <56982491+SandroPaulo@users.noreply.github.com>
2019-10-31 20:59:33 -0300

    Inclusão banner (index.php)

--- index.php
+++ index.php
@@ -13,9 +13,7 @@
     </style>
 </head>
 
-<body>
-
-
+<body >
 
     <?php
     include('includes/menu.php');
@@ -47,41 +45,49 @@
         </div>
     </div>
 
+    <article>
+        <div class="container-fluid">
 
+            <div class="row">
+                <div class="col-10">
 
+                    <div class="container linha-palco text-center cor1">
+                        <div class="row">
+                            <div class="col-sm-4 linha linha-palco1">
+                                <h1>Perfumes</h1>
+                                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                            </div>
 
+                            <div class="col-sm-4 linha borda-w">
+                                <h1>Marcas</h1>
+                                "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+                            </div>
+                            <div class="col-sm-4 linha linha-palco1">
+                                <h1>Ranking</h1>
+                                "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure.
+                            </div>
+                        </div>
+                    </div>
 
-    <div class="container linha-palco text-center cor1">
-        <div class="row">
-            <div class="col-sm-4 linha linha-palco1">
-                <h1>Perfumes</h1>
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                    <div class="embed-responsive embed-responsive-16by9 container padding">
+                        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/wCozFRta-jk"></iframe>
+                    </div>
+
+                </div>
+
+                <div class="col-2">
+
+                    <div class="sidebar-item">
+                        <div class="make-me-sticky">
+                            <img alt="" class="i-amphtml-fill-content i-amphtml-replaced-content" decoding="async" img src="assets\images\propaganda1.png">
+                        </div>
+                    </div>
+
+                </div>
             </div>
-
-            <div class="col-sm-4 linha borda-w">
-                <h1>Marcas</h1>
-                "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
-            </div>
-            <div class="col-sm-4 linha linha-palco1">
-                <h1>Ranking</h1>
-                "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure.
-            </div>
-
         </div>
 
-
-
-
-    </div>
-
-    <div class="embed-responsive embed-responsive-16by9 container padding">
-        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/wCozFRta-jk" allowfullscreen></iframe>
-    </div>
-
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
+    </article>
 
 
 
@@ -96,32 +102,20 @@
                         <h5>Inicio</h5>
                         <ul>
                             <li><a href="https://programadorviking.com.br/">Home</a></li>
-                            <li><a href="https://bit.ly/front-end-curso-completo-promocao-01">Cursos</a></li>
-                            <li><a href="https://programadorviking.com.br/">Downloads</a></li>
+                            <li><a href="https://bit.ly/front-end-curso-completo-promocao-01">Marcas</a></li>
+                            <li><a href="https://programadorviking.com.br/">Melhores perfumes</a></li>
                         </ul>
                     </div>
                     <div class="col-sm-2">
                         <h5>Sobre-nós</h5>
                         <ul>
-                            <li><a href="https://programadorviking.com.br/">Informações da Empresa</a></li>
+                            <li><a href="https://programadorviking.com.br/">Informações sobre o site</a></li>
                             <li><a href="https://programadorviking.com.br/">Contato</a></li>
-                            <li><a href="https://programadorviking.com.br/">Blog</a></li>
+
                         </ul>
                     </div>
-                    <div class="col-sm-2">
-                        <h5>Suporte</h5>
-                        <ul>
-                            <li><a href="https://programadorviking.com.br/">FAQ</a></li>
-                            <li><a href="https://programadorviking.com.br/">Telefones</a></li>
-                            <li><a href="https://programadorviking.com.br/">Chat</a></li>
-                        </ul>
-                    </div>
+
                     <div class="col-sm-3">
-                        <div class="social-networks">
-                            <a href="https://programadorviking.com.br/" class="twitter"><i class="fa fa-twitter"></i></a>
-                            <a href="https://programadorviking.com.br/" class="facebook"><i class="fa fa-facebook"></i></a>
-                            <a href="https://programadorviking.com.br/" class="instagram"><i class="fa fa-instagram"></i></a>
-                        </div>
                         <a href="https://programadorviking.com.br/">
                             <button type="button" class="btn btn-default">Contato</button>
                         </a>
@@ -129,10 +123,16 @@
                 </div>
             </div>
             <div class="footer-copyright">
-                <p>© 2019 Copyright - Programador Viking</p>
+                <p>© 2019 Copyright - Turma Programador WEB SENAC</p>
             </div>
         </footer>
     </div>
+
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
