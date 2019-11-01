6f9371b1f5ef361d11614f24d1cf914cd45cdd40
GabrielMsenac <56982472+GabrielMsenac@users.noreply.github.com>
2019-10-31 21:03:07 -0300

    formulario.php

--- pages/usuario/formulario.php
+++ pages/usuario/formulario.php
@@ -12,32 +12,35 @@
 
 <body>
     <div>
-    <form id="form-id" action="../../includes/executaform.php" method="post">
-        <div class="row margin1 text-center">
-            <div class="col-md-2">Nome <input type="text" name="nome"></div>
-            <div class="col-md-2">Sobrenome <input type="text" name="snome"></div>
-            <div class="col-md-4"></div>
-            <div class="col-md-4"></div>
-        </div>
-        <div class="row margin1">
-            <div class="col-md-4">E-mail <input type="text" name="email"></div>
-            <div class="col-md-3"></div>
-            <div class="col-md-3"></div>
-            <div class="col-md-2"></div>
-        </div>
-        <div class="row margin1">
-            <div class="col-md-3">Senha <input type="text" name="senha"></div>
-            <div class="col-md-3">Confirmar Senha <input type="text" name="csenha"></div>
-            <div class="col-md-3"></div>
-            <div class="col-md-3"></div>
-        </div>
-        <div class="row">
-            <div><a href="#" onclick="document.getElementById('form-id').submit();" class="col-md-12 btn btn-primary btn-lg" role="button">Salvar</a><br></div>
-        </div>
-        <div class="row">
-            <img src="../../assets/images/logo.png" class="col-md-12 img-fundo">
-        </div>
-        </div>
+        <form id="form-id" action="../../includes/executaform.php" method="post">
+            <div class="row margin1">
+                <div class="col-md-2 color-text1">Nome <input type="text" name="nome"></div>
+                <div class="col-md-2 color-text1">Sobrenome <input type="text" name="snome"></div>
+                <div class="col-md-4 color-text1"></div>
+                <div class="col-md-4 color-text1"></div>
+            </div>
+            <div class="row margin1">
+                <div class="col-md-4 color-text1">E-mail <input type="text" name="email"></div>
+                <div class="col-md-3 color-text1"></div>
+                <div class="col-md-3 color-text1"></div>
+                <div class="col-md-2 color-text1"></div>
+            </div>
+            <div class="row margin1">
+                <div class="col-md-3 color-text1">Senha <input type="text" name="senha"></div>
+                <div class="col-md-3 color-text1">Confirmar Senha <input type="text" name="csenha"></div>
+                <div class="col-md-3 color-text1"></div>
+                <div class="col-md-3 color-text1"></div>
+            </div>
+            <div class="row">
+                <div><a href="#" onclick="document.getElementById('form-id').submit();" class="col-md-12 btn btn-primary btn-lg" role="button">Salvar</a><br></div>
+            </div>
+            <div class="row">
+                <div class="col-md-3 color-text1">
+                    <img src="../../assets/images/logo.png" class="img-fundo">
+                </div>
+                
+            </div>
+    </div>
     </form>
 
 
