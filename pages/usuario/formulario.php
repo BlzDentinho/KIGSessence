95ae75dc8e199e614dc51ae9eb57375fa531df7f
GabrielMsenac <56982472+GabrielMsenac@users.noreply.github.com>
2019-10-31 22:10:34 -0300

    Update formulario.php
    pré finalização do formulario.php

--- pages/usuario/formulario.php
+++ pages/usuario/formulario.php
@@ -14,31 +14,33 @@
     <div class="container">
         <div>
             <div class="row">
+                <div class="col-md-0 color-text1"></div>
                 <div class="col-md-2 color-text1"></div>
-                <div class="col-md-8 color-text1"><img src="../../assets/images/logo.png" class="img-fundo1"></div>
+                <div class="col-md-6 color-text1 "><img src="../../assets/images/logo.png" class="img-fundo1"></div>
+                <div class="col-md-2 color-text1"></div>
                 <div class="col-md-2 color-text1"></div>
             </div>
             <form id="form-id" action="../../includes/executaform.php" method="post">
                 <div class="row margin1">
-                    <div class="col-md-4 color-text1">Nome <input type="text" name="nome" class="form-control"></div>
-                    <div class="col-md-4 color-text1">Sobrenome <input type="text" name="snome" class="form-control"></div>
-                    <div class="col-md-4 color-text1"></div>
-                    <div class="col-md-4 color-text1"></div>
-                </div>
-                <div class="row margin1">
-                    <div class="col-md-4 color-text1">E-mail <input type="text" name="email" class="form-control"></div>
-                    <div class="col-md-4 color-text1"></div>
-                    <div class="col-md-4 color-text1"></div>
-                    <div class="col-md-4 color-text1"></div>
-                </div>
-                <div class="row margin1">
-                    <div class="col-md-3 color-text1">Senha <input type="text" name="senha" class="form-control"></div>
-                    <div class="col-md-3 color-text1">Confirmar Senha <input type="text" name="csenha" class="form-control"></div>
+                <div class="col-md-3 color-text1"></div>    
+                <div class="col-md-3 color-text1">Nome <input type="text" name="nome" class="form-control shadow-sm"></div>
+                    <div class="col-md-3 color-text1">Sobrenome <input type="text" name="snome" class="form-control shadow-sm"></div>
                     <div class="col-md-3 color-text1"></div>
+                    
+                </div>
+                <div class="row margin1">
+                    <div class="col-md-3 color-text1"></div>
+                    <div class="col-md-6 color-text1">E-mail <input type="text" name="email" class="form-control shadow-sm"></div>
+                    <div class="col-md-3 color-text1"></div>
+                </div>
+                <div class="row margin1">
+                    <div class="col-md-3 color-text1"></div>
+                    <div class="col-md-3 color-text1">Senha <input type="text" name="senha" class="form-control shadow-sm"></div>
+                    <div class="col-md-3 color-text1">Confirmar Senha <input type="text" name="csenha" class="form-control shadow-sm"></div>
                     <div class="col-md-3 color-text1"></div>
                 </div>
                 <div class="row">
-                    <div class="col-md-4 color-text1"></div>
+                    <div class="col-md-6 color-text1"></div>
                     <div><a href="#" onclick="document.getElementById('form-id').submit();" class="btn btn-default" role="button">Salvar</a><br></div>
                     <div class="col-md-4 color-text1"></div>
                     <div class="col-md-4 color-text1"></div>
