002678144b921ca9639d1db0108329b52529a5f2
GabrielMsenac <56982472+GabrielMsenac@users.noreply.github.com>
2019-11-04 20:02:55 -0300

    Update formulario.php

--- pages/usuario/formulario.php
+++ pages/usuario/formulario.php
@@ -7,10 +7,13 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="../../assets/css/popup.css">
+    
+   
 
 </head>
 
 <body>
+
     <div class="container">
         <div>
             <div class="row">
