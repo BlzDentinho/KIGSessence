2aab724cb743b58d800b9aac782041c486ef5e7b
IVANCARLOD <56982455+IVANCARLOD@users.noreply.github.com>
2019-11-07 19:44:45 -0300

    Footer corrigido

--- includes/footer.php
+++ includes/footer.php
@@ -4,7 +4,7 @@
 <head>
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-<link rel="stylesheet" href="../../css/home.css">
+<link rel="stylesheet" href="../assets/css/home.css">
 
 
 </head>
