c73331417a185e3ea8fba2f704651fa58ad6f5f1
BlzDentinho <56982450+BlzDentinho@users.noreply.github.com>
2019-11-06 21:08:41 -0300

    att foto
    att foto

--- pages/perfume/telaperfume.php
+++ pages/perfume/telaperfume.php
@@ -1,11 +1,30 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        
-    </head> 
-    <body>
 
-    </body>   
-  
+<head>
+    <title>KIGSessence</title>
+    <meta charset="utf-8">
 
-</html>    
+    <?php include("../../includes/header.php"); ?>
+
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+            <div class="text-center">
+                    <img src="../../assets/images/perfume1.png" height="500"/>
+                </div>
+            </div>
+            <div class="col-sm-4">
+            </div>
+            <div class="col-sm-4">
+            </div>
+        </div>
+    </div>
+
+</body>
+
+
+</html>
