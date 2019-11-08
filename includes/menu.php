f561c7f3c7c768e98a5ae7f14e7d4de8d2e28e75
GabrielMsenac <56982472+GabrielMsenac@users.noreply.github.com>
2019-11-07 21:43:05 -0300

    Update menu.php

--- includes/menu.php
+++ includes/menu.php
@@ -32,7 +32,7 @@
                 </form>
             </div>
             <div class="col-md-2 linha borda-w margin2">
-            <button  type="button"  onclick="abrir()" class="btn btn-success">Login</button>
+            <button type="button" class="btn btn-success">Login</button>
             </div>
         </div>
     </div>
