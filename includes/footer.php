aa9d229ffc686b0ef3e78ec968eff6fb401d01ab
GabrielMsenac <56982472+GabrielMsenac@users.noreply.github.com>
2019-11-18 21:12:45 -0300

    test button

--- includes/footer.php
+++ includes/footer.php
@@ -5,6 +5,7 @@
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 <link rel="stylesheet" href="../assets/css/home.css">
+<link rel="stylesheet" href="../assets/css/popup.css">
 
 
 </head>
