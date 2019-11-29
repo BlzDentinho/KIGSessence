6dd2769ac0f6130fabd35b1f6ef6d9741f44bca0
SandroPaulo <56982491+SandroPaulo@users.noreply.github.com>
2019-11-28 21:13:23 -0300

    Update novidades.php

--- novidades.php
+++ novidades.php
@@ -19,49 +19,52 @@
 </head>
 
 <body style="overflow-x:hidden">
-    
-<?php
+
+    <?php
     include('includes/menu.php');
     ?>
 
-<div class="container-fluid">
+    <div class="container-fluid">
 
-<div class="row">
-    <div class="col-10">
+        <div class="row">
+            <div class="col-10">
 
-        <div class="container linha-palco cor1">
-            <div class="row">
-                <div class="col-sm-12 text-center linha-palco1">
-                    <h1>Invictus by Paco Rabanne </h1>
-                    <p>"Verdade. Lealdade. Amor. Celebre uma nova era de masculinidade com K by Dolce & Gabbana. Um Eau de Toilette envolvente que une tradição e modernidade.
-                        A fragrância abre com uma mistura fresca e energética de Laranja Sanguínea, Limão Siciliano e Bagas de Zimbro. No coração, um toque quente e refinado de Essência Picante de Pimento, Âmbar de Sálvia, Gerânio e Lavanda. Por fim, um rastro magnético e inesquecível de Cedro, Vetiver e Patchouli.
-                        O frasco apresenta uma tampa artesanal, geometricamente esculpida em forma de coroa, com partículas de ouro de 24 quilates. A letra K evoca o símbolo tradicional da realeza, mas com uma simplicidade ousada que é surpreendentemente contemporânea"<br>Consumidores ao redor do mundo continuam “in love” pelos doces perfumes doces, de forte apelo afetivo, e a proximidade entre perfumes e sabores continua possibilitando inúmeras iniciativas que mantem este universo entre os mais dinâmicos e inovadores na perfumaria. Um exemplo disso é a abertura do bar de Fragrâncias no Ritz Carlton em Berlin. A seleção de coquetéis homenageia alguns dos perfumes mais icônicos do mundo: Giorgio Armani, L’Artisan Parfumeur, Annick Goutal, Yves Saint Laurent, Salvatore Ferragamo e Guerlain.</p>
+                <div class="container linha-palco cor1 float-left">
+                    <div class="row">
+                        <div class="col-sm-12 text-center linha-palco1">
+                            <h1>Invictus by Paco Rabanne </h1>
+                            <p>"Verdade. Lealdade. Amor. Celebre uma nova era de masculinidade com K by Dolce & Gabbana. Um Eau de Toilette envolvente que une tradição e modernidade.
+                                A fragrância abre com uma mistura fresca e energética de Laranja Sanguínea, Limão Siciliano e Bagas de Zimbro. No coração, um toque quente e refinado de Essência Picante de Pimento, Âmbar de Sálvia, Gerânio e Lavanda. Por fim, um rastro magnético e inesquecível de Cedro, Vetiver e Patchouli.
+                                O frasco apresenta uma tampa artesanal, geometricamente esculpida em forma de coroa, com partículas de ouro de 24 quilates. A letra K evoca o símbolo tradicional da realeza, mas com uma simplicidade ousada que é surpreendentemente contemporânea"<br>Consumidores ao redor do mundo continuam “in love” pelos doces perfumes doces, de forte apelo afetivo, e a proximidade entre perfumes e sabores continua possibilitando inúmeras iniciativas que mantem este universo entre os mais dinâmicos e inovadores na perfumaria. Um exemplo disso é a abertura do bar de Fragrâncias no Ritz Carlton em Berlin. A seleção de coquetéis homenageia alguns dos perfumes mais icônicos do mundo: Giorgio Armani, L’Artisan Parfumeur, Annick Goutal, Yves Saint Laurent, Salvatore Ferragamo e Guerlain.<br></p>
+                        </div>
+                    </div>
                 </div>
 
-                          </div>
-        </div>
+                <div class="card linha-palco" style="width: 18rem;">
+                    <img class="card-img-top" src="assets\images\invictus.png" alt="Card image cap">
+                </div>
 
-        <div class="text-center padding">
-            <iframe width="700" height="400" src="https://www.youtube.com/embed/VNOZTsK0gRs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        </div>
+                <div class="text-center padding">
+                    <iframe width="700" height="400" src="https://www.youtube.com/embed/VNOZTsK0gRs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
 
-    </div>
-
-    <div class="col-2">
-
-        <div class="sidebar-item">
-            <div class="make-me-sticky">
-                <img alt="" class="i-amphtml-fill-content i-amphtml-replaced-content" decoding="async" img src="assets\images\propaganda1.png">
             </div>
-        </div>
 
+            <div class="col-2">
+
+                <div class="sidebar-item">
+                    <div class="make-me-sticky">
+                        <img alt="" class="i-amphtml-fill-content i-amphtml-replaced-content" decoding="async" img src="assets\images\propaganda1.png">
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
     </div>
 
-</div>
-</div>
 
-
-<?php include("includes/footer.php"); ?>
+    <?php include("includes/footer.php"); ?>
 
 
 
@@ -71,4 +74,3 @@
 </body>
 
 </html>
-
